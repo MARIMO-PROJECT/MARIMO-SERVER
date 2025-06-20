c1a8c488c20fa9d92fb06d841b023d3b14521a14
ckkim817 <ckkim817@konkuk.ac.kr>
2025-06-20 16:58:34 +0900

    hotfix: null인 필드는 응답으로 보내지 않도록 수정

--- src/main/java/com/marimo/server/domain/product/dto/BannerResponse.java
+++ src/main/java/com/marimo/server/domain/product/dto/BannerResponse.java
@@ -1,5 +1,9 @@
 package com.marimo.server.domain.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public record BannerResponse(
         String fileUrl,
         String ctaUrl
