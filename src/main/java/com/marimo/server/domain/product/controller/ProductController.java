586fd3b74ad0cb796eba8b41a569ecfb670ae8dc
ckkim817 <ckkim817@konkuk.ac.kr>
2025-06-27 18:53:49 +0900

    feat: 청첩장 상세페이지 조회 API 엔드포인트 작성 (#7)

--- src/main/java/com/marimo/server/domain/product/controller/ProductController.java
+++ src/main/java/com/marimo/server/domain/product/controller/ProductController.java
@@ -1,15 +1,19 @@
 package com.marimo.server.domain.product.controller;
 
 import com.marimo.server.domain.product.dto.BannerListResponse;
+import com.marimo.server.domain.product.dto.InvitationDetailResponse;
 import com.marimo.server.domain.product.dto.InvitationListResponse;
 import com.marimo.server.domain.product.enums.ProductType;
 import com.marimo.server.domain.product.service.ProductService;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,17 @@ public class ProductController {
     public ResponseEntity<InvitationListResponse> getInvitations() {
         return ResponseEntity.ok(
                 productService.fetchInvitations()
+        );
+    }
+
+    @GetMapping(path = "/invitations/{invitationId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<InvitationDetailResponse> getInvitationDetail(
+            @NotNull(message = "invitationId는 필수입니다.")
+            @Positive(message = "invitationId는 양수여야 합니다.")
+            @PathVariable(name = "invitationId") final Long invitationId
+    ) {
+        return ResponseEntity.ok(
+                productService.fetchInvitationDetail(invitationId)
         );
     }
 }
